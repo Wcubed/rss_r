3bd87acf6240f85ed912cc2b09804683105131df
Wybe Westra <dev@wwestra.nl>
2022-07-10 10:00:05 +0200

    Shared mutable counter.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,28 @@
 #![warn(rust_2018_idioms)]
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello_world))
+    let counter = web::Data::new(AppStateCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || App::new().app_data(counter.clone()).service(hello_world))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
 
 #[get("/")]
-async fn hello_world() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
+async fn hello_world(data: web::Data<AppStateCounter>) -> impl Responder {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    HttpResponse::Ok().body(format!("Hello world! Counter: {counter}"))
+}
+
+struct AppStateCounter {
+    counter: Mutex<i32>,
 }
